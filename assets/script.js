ebccc63712ab433a1bd4f16f4286a9975d07c600
Chelsey Morris <chelseymorris91@yahoo.com>
2021-02-23 17:02:55 -0800

    able to show search results onto page

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,36 @@
-// in search area user types in info
-// we need to take the value of that info and add it to the api url
-// then we can make our fetch request 
-// it will return all of the api's with the seacrh criteria
-// figure out how to get it onto screen
-// and what we want to show of that info
-
-
+var searchResultsElement = $(".search-results");
 var searchInput = $('#search-area');
 var searchBtnElement = $('.searchbtn');
-
 searchBtnElement.on("click", function(event) {
     event.preventDefault();
     var userInput = searchInput.val();
-    var baseUrl = "https://www.loc.gov/search?q=" + userInput;
-    console.log(baseUrl)
-    
+    var baseUrl = "https://www.loc.gov/search?fo=json&per_page=10&q=" + userInput;
+    console.log(baseUrl);
+    fetch(baseUrl) 
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function(data) {
+          var myData = data.results
+          for (var i = 0; i< myData.length; i++ ) {
+            console.log(myData[i])
+            var myTitle = $("<h1>");
+            var myDate = $("<h2>");
+            var myDescription = $("<h3>");
+            var myUrl = $("<h4>");
+            myTitle.text(myData[i].title);
+            myDate.text(myData[i].date);
+            myDescription.text(myData[i].description[0]);
+            myUrl.text(myData[i].url);
+            searchResultsElement.append(myTitle);
+            searchResultsElement.append(myDate);
+            searchResultsElement.append(myDescription);
+            searchResultsElement.append(myUrl);
+            
+          }
+      });
 })
+
+
+
+
